8ae33731d79f517d5dcd3c8ae2fd92836a6c7914
jonasborgesweb <jonasborgesweb@gmail.com>
2020-05-19 10:47:31 -0300

    Updates

---
+++ precache-manifest.610dfbc42ca0cf9afbd6c70be030c940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a950021d3cb893494cfdaa0b91413963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3d1973d63cab27998a9",
+    "url": "/static/js/2.e6b7918d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6b7918d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56853b06fae17548a932",
+    "url": "/static/js/main.6ded716e.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  }
+]);
